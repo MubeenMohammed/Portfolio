e7bc07e187e0e7ee39aed26098de13bba98d266b
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-06-10 02:07:38 -0400

    added all the projects

--- scripts/index.js
+++ scripts/index.js
@@ -141,5 +141,12 @@ educationButtonElement.addEventListener("click", () => {
         `;
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
